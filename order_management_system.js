de20ae7d25137daec481bd16bbe992d250c93e1f
roman deblase <rdeblase2000@gmail.com>
2024-09-22 22:43:36 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,10 @@
+{
+    var inventory = [
+        {name: "coffee", price: 3, quantity: 50},
+        {name: "espresso", price: 2, quantity: 50},
+        {name: "cold brew", price: 5, quantity: 50},
+        {name: "pastry", price: 4, quantity: 20},
+    ];
+//initialize inventory with product objects
+
+}
